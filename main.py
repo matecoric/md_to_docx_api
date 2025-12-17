f82385cb65a0078db797cf887bda29eee66bbb25
matecoric <mate.coric@q.agency>
2025-12-17 11:14:27 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -15,8 +15,19 @@ def markdown_to_docx(markdown_string: str = Body(..., media_type="text/plain")):
     with tempfile.TemporaryDirectory() as tmpdir:
         output_path = Path(tmpdir) / "output.docx"
 
-        pypandoc.convert_text(
-            source=markdown_string,
+        md_path = Path(tmpdir) / "input.md"
+        md_path.write_text(markdown_string, encoding="utf-8")
+
+        # pypandoc.convert_text(
+        #     source=markdown_string,
+        #     to="docx",
+        #     format="md",
+        #     outputfile=str(output_path),
+        #     extra_args=[f"--reference-doc={TEMPLATE_PATH}"],
+        # )
+
+        pypandoc.convert_file(
+            str(md_path),
             to="docx",
             format="md",
             outputfile=str(output_path),
